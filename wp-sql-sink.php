07d631d00ea905256f4916958cafbc4552c6696d
AbiusX <git@abiusx.com>
2014-01-17 18:22:57 -0500

    sample sink code that is used by WP-SQLI-LAB

--- wp-sql-sink.php
+++ wp-sql-sink.php
@@ -3,10 +3,57 @@
  * this script holds the sink handlers. 
  * 
  */
+
+
+global $inputs;
+$inputs=array();
+foreach ($_GET as $k=>$v)
+	$inputs[0][$k]=$v;
+foreach ($_GET as $k=>$v)
+	$inputs[1][$k]=$v;
+global $file;
+$file="/tmp/logs/sql";
+function store_data()
+{
+	global $file;
+	$list=glob($file."*");
+	sort($list);
+
+	$t=preg_match('/(\d+)\.{1,5}.*$/', end($list), $matches);
+	if ($t)
+		$number=$matches[1];
+	else
+		$number="0";
+	$number=str_pad($number+1, 3,"0",STR_PAD_LEFT);
+	rename($file.".txt", $file.$number.".txt");
+}
+register_shutdown_function("store_data");
+function dump_log($in,$out,$GET=true)
+{
+	global $file;
+	$f=$file.".txt";
+	if ($GET)
+		$GET="GET";
+	else
+		$GET="POST";
+	$data="IN {$GET} 1 ".strlen($in)." ".$in.PHP_EOL;
+	file_put_contents($f, $data,FILE_APPEND);
+	$data="OUT sql 1 ".strlen($out)." ".$out.PHP_EOL;
+	file_put_contents($f, $data,FILE_APPEND);
+}
 function sink_handler($query)
 {
+	global $inputs;
+	foreach ($inputs[0] as $input) //get
+		dump_log($input,$query);
+	foreach ($inputs[1] as $input) //post
+		dump_log($input,$query,false);
+	if (count($inputs[0])==0 and count($inputs[1])==0)
+		dump_log("",$query);
 	//put your sink handling logic here
 }
+
+
 class PDO_ extends PDO
 {
 
